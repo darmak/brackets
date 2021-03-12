09d7b1aaff26cef094aa91c90393836c6fe115e7
Dmitry Buravkin <buravkin.08@mail.ru>
2021-03-12 14:49:29 +0300

    init:start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let mapArr = new Map(bracketsConfig);
+    const arr = [];
+
+    for (let char of str) {
+        
+
+        if (char === mapArr.get(arr[arr.length - 1])) arr.pop();
+        else arr.push(char);
+    }
+
+    return !arr.length;
+  
 }
